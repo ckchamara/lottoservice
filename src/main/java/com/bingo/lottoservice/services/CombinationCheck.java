3f21e8d31db501722fa3d6fbf0c0bd3f769c2592
Chamara <ckchamara22@gmail.com>
2020-01-06 00:52:00 +0530

    read map value logic fixed

--- src/main/java/com/bingo/lottoservice/services/CombinationCheck.java
+++ src/main/java/com/bingo/lottoservice/services/CombinationCheck.java
@@ -5,18 +5,17 @@ import com.bingo.lottoservice.model.Lottery;
 import com.bingo.lottoservice.model.Result;
 import com.bingo.lottoservice.model.Rule;
 import com.bingo.lottoservice.utils.LoadYAML;
+import org.apache.commons.lang3.math.NumberUtils;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 
 public class CombinationCheck {
 
     public CombinationCheck() throws IOException {
     }
 
-    Configuration configuration = LoadYAML.load(Configuration.class,"test.yml");
+    Configuration configuration = LoadYAML.load(Configuration.class, "configuration.yml");
     Result result = LoadYAML.load(Result.class,"result.yml");
     Lottery lottery = LoadYAML.load(Lottery.class,"lottery.yml");
 
@@ -26,16 +25,24 @@ public class CombinationCheck {
         System.out.println(lottery.getName());
     }
 
-    public void checkReward(){
-       ArrayList<LinkedHashMap<Integer,Object>> resultpositions = result.getPositions();
-        for (LinkedHashMap<Integer,Object> position: resultpositions) {
-            System.out.println(position.values()+" "+" "+position.keySet());
-            int position1 = 5;
-            if (compareLottreryResultMatch(position.get(position1),position1)){
-                System.out.println("matched successfully");
+    public void checkReward() throws NullPointerException{
+        //get config rule
+        for (Rule rule:configuration.getRules()) {
+            System.out.println(rule.getRule());
+            //get rule positions
+            for (int rulePosition:rule.getPositions()) {
+                //get result position
+                ArrayList<LinkedHashMap<Integer, Object>> resultPositions = result.getPositions();
+                //get lotteryPositions
+                ArrayList<LinkedHashMap<Integer, Object>> lotteryPositions = lottery.getPositions();
+                //check wether resultPosition match to loteryPositoin
+                LinkedHashMap<Integer,Object> avc = merge133(lotteryPositions);
+                System.out.println(merge133(lotteryPositions));
+                //for loop to retrive LinkedHashMap
+
             }
-            else System.out.println("not matched");
         }
+
     }
 
     public boolean compareLottreryResultMatch(Object resultValue,int resultPosition){
@@ -48,4 +55,30 @@ public class CombinationCheck {
         return false;
     }
 
+    //combine all the LinkedHashMapa into one
+    public List<Integer> extractMapValuesToList(ArrayList<LinkedHashMap<Integer, Object>> mapList){
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (LinkedHashMap<Integer, Object> map:mapList) {
+            map.values().stream()
+                    .filter(e -> NumberUtils.isCreatable(e.toString()))
+                    .forEach(e -> arrayList.add(Integer.valueOf(e.toString())));
+        }
+        return arrayList;
+    }
+
+    public LinkedHashMap<Integer,Object> merge(ArrayList<LinkedHashMap<Integer, Object>> mapList){
+        LinkedHashMap<Integer, Object> mergeMap = new LinkedHashMap<>();
+        for (LinkedHashMap<Integer,Object> singleMap:mapList) {
+            mergeMap.putAll(singleMap);
+        }
+       return mergeMap;
+    }
+
+    public <T,V> LinkedHashMap<T,V> merge133(ArrayList<LinkedHashMap<T, V>> mapList){
+        LinkedHashMap<T, V> mergeMap = new LinkedHashMap<>();
+        for (LinkedHashMap<T,V> singleMap:mapList) {
+            mergeMap.putAll(singleMap);
+        }
+        return mergeMap;
+    }
 }
