51de3eca2ae9077fd0a87e0a6615198e1351e627
ckchamara <ckchamara22@gmail.com>
2020-01-21 23:08:00 +0530

    added folderWise yml read

--- src/main/java/com/bingo/lottoservice/utils/LoadYAML.java
+++ src/main/java/com/bingo/lottoservice/utils/LoadYAML.java
@@ -3,18 +3,20 @@ package com.bingo.lottoservice.utils;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
-import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 
 public class LoadYAML {
     public static <T> T load(Class clz, Class<T> myClass, String ymlFilePath) throws IOException, URISyntaxException {
-        Path filePath = Paths.get(clz.getClassLoader().getResource(ymlFilePath).toURI()).toAbsolutePath().getFileName();
+        Path filePath = Paths.get(Objects.requireNonNull(clz.getClassLoader().getResource(ymlFilePath)).toURI());
         Yaml yaml = new Yaml(new Constructor(myClass));
-        InputStream inputStream = new ByteArrayInputStream(new FileReader().getFileContent(filePath.toString()).getBytes());
+        //InputStream inputStream = new ByteArrayInputStream(new FileReader().getFileContent(filePath.toString()).getBytes());
+        InputStream inputStream = new FileInputStream(filePath.toFile());
         return yaml.load(inputStream);
     }
 
