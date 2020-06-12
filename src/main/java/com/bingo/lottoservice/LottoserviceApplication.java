83cd1e874dd9bb0e2a51e1a4a2eced118b9d04c9
ckchamara <ckchamara22@gmail.com>
2020-06-13 01:16:26 +0530

    propety file read by a single class created

--- src/main/java/com/bingo/lottoservice/LottoserviceApplication.java
+++ src/main/java/com/bingo/lottoservice/LottoserviceApplication.java
@@ -1,23 +1,12 @@
 package com.bingo.lottoservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Service;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
 
 @SpringBootApplication
 public class LottoserviceApplication {
 
 	public static void main(String[] args)  {
 		SpringApplication.run(LottoserviceApplication.class, args);
-//		CombinationCheck combinationCheck = new CombinationCheck();
-		try {
-//			combinationCheck.checkReward();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 }
